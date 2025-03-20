1ebc5d0b3167d9803a58c25194ef69b973409844
rafssjesus <148909191+rafssjesus@users.noreply.github.com>
2025-03-20 15:06:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const linhaDoTempo = document.getElementById("linha-do-tempo");
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao document.getElementById('diminuir-fonte');
 
-const itens = [
-    { ano: 2020, descricao: "Item 1" },
-    { ano: 2021, descricao: "Item 2" },
-    { ano: 2022, descricao: "Item 3" },
-];
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-itens.forEach((item) => {
-    const li = document.createElement("li");
-    li.innerHTML = `
-        <h2>${item.ano}</h2>
-        <p>${item.descricao}</p>
-    `;
-    linhaDoTempo.appendChild(li);
-});
+    })
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+})
