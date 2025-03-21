26d2ea3a556e065575b7675c1158b8799976f3d5
rafssjesus <148909191+rafssjesus@users.noreply.github.com>
2025-03-21 15:02:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+document.getElementById('aumentar-fonte').addEventListener('click', function() {
+    document.body.style.fontSize = 'larger';
+});
 
-    botaoDeAcessibilidade.addEventListener('click', function () {
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+document.getElementById('diminuir-fonte').addEventListener('click', function() {
+    document.body.style.fontSize = 'smaller';
+});
 
-    })
-
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
-    const alternaContraste = document.getElementById('alterna-contraste')
-
-    let tamanhoAtualFonte = 1;
-
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-
-    alternaContraste.addEventListener('click', function () {
-        document.body.classList.toggle('alto-contraste')
-    })
-
-
-})
-
-ScrollReveal().reveal('#inicio', { delay: 500 });
-ScrollReveal().reveal('#tropicalia', { delay: 500 });
-ScrollReveal().reveal('#galeria', { delay: 500 });
-ScrollReveal().reveal('#contato', { delay: 500 });
+document.getElementById('botao-acessibilidade').addEventListener('click', function() {
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    opcoesAcessibilidade.classList.toggle('d-none'); // Alterna a visibilidade das opções
+});
